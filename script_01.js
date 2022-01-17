874052a80174324cbe9b2586560c14f191d07785
javad290 <javad290@gmail.com>
2022-01-17 11:14:38 +0100

    Beispiel 1

--- script_01.js
+++ script_01.js
@@ -17,11 +17,62 @@
 
 //***02 Deklaration + Wertzuweisung I */
 
-let firstName; //Deklaration (Definition)
-firstName = "Javad"; // Wertzuweisung
-console.log(firstName); //Ausgabe
+// let firstName; //Deklaration (Definition)
+// firstName = "Javad"; // Wertzuweisung
+// console.log(firstName); //Ausgabe
 
-let familyName = "Hajian" //Deklaration + Wertzuweisung
-console.log(familyName);
-console.log(firstName + " " + familyName);
+// let familyName = "Hajian" //Deklaration + Wertzuweisung
+// console.log(familyName);
+// console.log(firstName + " " + familyName);
+
+
+//   03 Deklaration + Wertzuweisung II **
+
+// let firstName, familyName;
+// firstName = prompt("Bitte Vorname eingeben");
+// familyName = prompt("Bitte NachName eingeben")
+// console.log(firstName + " " + familyName);
+
+
+// let test
+// test = ("HI")
+// // test = 2
+// // test = true
+
+// console.log("Datentyp : " + typeof test);
+// console.log("Inhalt : " + test);
+
+
+//*** 03a Konstanten */
+
+// const test = 2 // Variable باید همزمان با معرفی مقار هم داده شود
+// //  test = "Hi"  // مقدار کانست و نمیشه عوض کرد
+
+// console.log("Inhalt" + " : "+  test);
+
+// ****beispiel *********************
+//Deklaration
+
+// let ageJohn, ageMark
+// let birthYearJohn, birthYearMark
+
+// //Year Funktion
+// let date = new Date()
+// let year =date.getFullYear()
+
+// //Wertzuweisung
+
+// ageJohn = 25
+// ageMark = 30
+
+// //Berechnung
+// birthYearJohn = year - ageJohn
+// birthYearMark = year - ageMark
+
+// //Ausgabe
+// console.log("birthYearJohn: " +   birthYearJohn);
+// console.log("birthYearMark: " +   birthYearMark);
+
+//******************************************* */
+
 
